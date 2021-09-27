26df42e8dce9a71193cdd92c2cb116ad87885414
Luis Felipe Evilla <luisevilla588@gmail.com>
2021-09-26 19:14:58 -0500

    corrigiendo error en asignacion de puerto

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const PORT_SERVER = process.env.PORT_SERVER || 3000
+const PORT_SERVER = process.env.PORT || 3000
 const DB_FILE = process.env.DB_FILE || "./bienestar.db"
 
 module.exports = {
