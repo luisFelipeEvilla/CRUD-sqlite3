a4d18c9125ed34a10b70cc0245314508ccfa0f96
Luis Felipe Evilla <luisevilla588@gmail.com>
2021-09-28 11:46:14 -0500

    modificando envio de hijos

--- routes/hijos.js
+++ routes/hijos.js
@@ -3,62 +3,65 @@ const { db } = require('../db/index');
 const { post } = require('./padres');
 
 router.get('/', (req, res) => {
-    db.serialize(()=> {
-        db.all(`SELECT * FROM \"Hijos\"`, (err, rows) => {
-        if (err) {
-          console.error(err.message);
-        }
-        res.send(rows)
-      });
-    })
+  const query = `SELECT h.id, h.nombre, p.id as idPadre,
+     p.nombre as nombrePadre FROM "Hijos" h INNER JOIN "Padres" p ON (hijode = p.id)`;
+
+  db.serialize(() => {
+    db.all(query, (err, rows) => {
+      if (err) {
+        console.error(err.message);
+      }
+      res.send(rows)
+    });
+  })
 })
 
 router.get('/sinpadre', (req, res) => {
-    db.serialize(()=> {
-        db.all(`SELECT * FROM \"Hijos\" WHERE hijode IS NULL`, (err, rows) => {
-        if (err) {
-          console.error(err.message);
-          return res.send("Error de parte del servidor").status(500)
-        }
+  db.serialize(() => {
+    db.all(`SELECT * FROM \"Hijos\" WHERE hijode IS NULL`, (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        return res.send("Error de parte del servidor").status(500)
+      }
 
-        res.send(rows).status(200)
-      });
-    })
+      res.send(rows).status(200)
+    });
+  })
 })
 
 router.post('/', (req, res) => {
 
-    const {
-        nombre,
-        hijode
-    } = req.body;
+  const {
+    nombre,
+    hijode
+  } = req.body;
 
-    const query = `INSERT INTO "Hijos" (nombre, hijode) VALUES(?, ?);`;
-    const params = [nombre, hijode]
+  const query = `INSERT INTO "Hijos" (nombre, hijode) VALUES(?, ?);`;
+  const params = [nombre, hijode]
 
-    db.serialize(()=> {
-        db.run(query, params, (err,rows) => {
-        if (err) {
-          console.log(err);
-          return res.send("Error Creando el recuro").status(500)
-        }
+  db.serialize(() => {
+    db.run(query, params, (err, rows) => {
+      if (err) {
+        console.log(err);
+        return res.send("Error Creando el recuro").status(500)
+      }
 
-        res.send("Recurso creado correctamente").status(200)
-      });
-    })
+      res.send("Recurso creado correctamente").status(200)
+    });
+  })
 })
 
-router.get('/:id', (req,res) => {
+router.get('/:id', (req, res) => {
 
   const id = req.params.id
 
   const query = `SELECT * FROM Hijos WHERE id = ?`;
   const params = [id]
 
-  db.get(query,params, (err, row) => {
+  db.get(query, params, (err, row) => {
     if (err) {
       return res.send(`Error del lado del servidor`).status(500)
-    } 
+    }
 
     if (row == null) {
       return res.send(`Error, el hijo con el id ${id} no existe`).status(404)
@@ -72,55 +75,55 @@ router.get('/:id', (req,res) => {
     db.get(query, params, (err, row) => {
       if (err) {
         return res.send(`Error del lado del servidor`).status(500)
-      } 
+      }
 
       hijo.padre = row;
 
       res.send(hijo).status(200)
     })
-   })
+  })
 })
 
 router.put('/:id', (req, res) => {
 
-    const id = req.params.id
+  const id = req.params.id
 
-    const {
-        nombre,
-        hijode
-    } = req.body;
+  const {
+    nombre,
+    hijode
+  } = req.body;
 
-    const query = `UPDATE "Hijos" set nombre=?, hijode=? WHERE id=?;`;
-    const params = [nombre, hijode, id]
+  const query = `UPDATE "Hijos" set nombre=?, hijode=? WHERE id=?;`;
+  const params = [nombre, hijode, id]
 
-    db.serialize(()=> {
-        db.run(query, params, (err,rows) => {
-        if (err) {
-            console.log(err);
-          return res.send("Error actualizando el recurso").status(500)
-        }
+  db.serialize(() => {
+    db.run(query, params, (err, rows) => {
+      if (err) {
+        console.log(err);
+        return res.send("Error actualizando el recurso").status(500)
+      }
 
-        res.send("Recurso actualizado correctamente").status(200)
-      });
-    })
+      res.send("Recurso actualizado correctamente").status(200)
+    });
+  })
 })
 
 router.delete('/:id', (req, res) => {
-    const id = req.params.id
+  const id = req.params.id
 
-    const query = `DELETE FROM "Hijos" WHERE id=?;`;
-    const params = [id];
+  const query = `DELETE FROM "Hijos" WHERE id=?;`;
+  const params = [id];
 
-    db.serialize(()=> {
-        db.run(query, params, (err,rows) => {
-        if (err) {
-            console.log(err);
-           return res.send("Error Eliminando el recurso").status(500)
-        }
+  db.serialize(() => {
+    db.run(query, params, (err, rows) => {
+      if (err) {
+        console.log(err);
+        return res.send("Error Eliminando el recurso").status(500)
+      }
 
-        res.send("Recurso eliminado correctamente").status(200)
-      });
-    })
+      res.send("Recurso eliminado correctamente").status(200)
+    });
+  })
 })
 
 module.exports = router
